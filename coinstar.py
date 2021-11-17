7a6751f7c6b50047838a16eb6ea3eb9b69d45a4d
JValtteri <46611993+JValtteri@users.noreply.github.com>
2021-11-17 23:18:41 +0200

    Basic structure for the program

---
+++ coinstar.py
@@ -0,0 +1,17 @@
+class Market():
+    pass
+
+def bearish_length(self):
+    pass
+
+def find_max_volume(self):
+    pass
+
+def best_buy(self):
+    pass
+
+def best_sell(self):
+    pass
+
+def https_getter(self):
+    pass
