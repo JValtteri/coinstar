c659ad020187d9875c229279f421b31b3939f392
JValtteri <46611993+JValtteri@users.noreply.github.com>
2021-11-22 19:00:38 +0200

    timestamp

--- coinstar.py
+++ coinstar.py
@@ -1,5 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# J.V.Ojala 17.11.2021
+# coinstar
+
+import time, datetime
+import requests
+
 class Market():
-    pass
+
+    def __init__(self, time_from, time_to, coin='bitcoin', currency='eur'):
+
+        self.time_from
+        self.time_to,
+        self.coin='bitcoin'
+        self.currency='eur'
 
 def bearish_length(self):
     pass
@@ -14,4 +28,17 @@ def best_sell(self):
     pass
 
 def https_getter(self):
+
+    address = f"https://api.coingecko.com/api/v3/coins/{self.coin}/market_chart/range"\
+              f"?vs_currency={self.currency}&from={self.time_from}&to={self.time_to}"
+    r = requests.get(address)
+    r.json()
+
+def time_to_posix(year, mon, day, hour=0, min=0, sec=0):
+    """Takes date and time and returns POSIX timestamp in seconds"""
+    time_object = datetime.datetime(year, mon, day, hour, min, sec)
+    timestamp = round( time_object.timestamp() )
+    return timestamp
+
+if __name__ == "__main__":
     pass
