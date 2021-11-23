1d3d1c13e77c273d51c0ac4b5b2723343345a3a3
JValtteri <46611993+JValtteri@users.noreply.github.com>
2021-11-23 21:49:23 +0200

    HTTP GET

--- coinstar.py
+++ coinstar.py
@@ -10,29 +10,35 @@ class Market():
 
     def __init__(self, time_from, time_to, coin='bitcoin', currency='eur'):
 
-        self.time_from
-        self.time_to,
-        self.coin='bitcoin'
-        self.currency='eur'
+        self.time_from = time_from
+        self.time_to = time_to
+        self.coin = coin
+        self.currency = currency
 
-def bearish_length(self):
-    pass
+        # Get market data
+        self.transactions = self.https_getter()
 
-def find_max_volume(self):
-    pass
+    def bearish_length(self):
+        pass
 
-def best_buy(self):
-    pass
+    def is_bearish(self):
+        pass
 
-def best_sell(self):
-    pass
+    def find_max_volume(self):
+        pass
 
-def https_getter(self):
+    def best_buy(self):
+        pass
 
-    address = f"https://api.coingecko.com/api/v3/coins/{self.coin}/market_chart/range"\
-              f"?vs_currency={self.currency}&from={self.time_from}&to={self.time_to}"
-    r = requests.get(address)
-    r.json()
+    def best_sell(self):
+        pass
+
+    def https_getter(self):
+
+        address = f"https://api.coingecko.com/api/v3/coins/{self.coin}/market_chart/range"\
+                f"?vs_currency={self.currency}&from={self.time_from}&to={self.time_to}"
+        r = requests.get(address)
+        return r.json()
 
 def time_to_posix(year, mon, day, hour=0, min=0, sec=0):
     """Takes date and time and returns POSIX timestamp in seconds"""
@@ -41,4 +47,11 @@ def time_to_posix(year, mon, day, hour=0, min=0, sec=0):
     return timestamp
 
 if __name__ == "__main__":
+    bitcoin_market = Market(
+        time_from = time_to_posix(2021, 11, 22, hour=0, min=0, sec=0),
+        time_to = time_to_posix(2021, 11, 22, hour=12, min=0, sec=0),
+        coin="bitcoin",
+        currency="eur"
+    )
+
     pass
