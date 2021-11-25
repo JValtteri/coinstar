9283c2e95739d541e32028c085be417dedd50d2d
JValtteri <46611993+JValtteri@users.noreply.github.com>
2021-11-25 22:33:20 +0200

    Working Market_day() Class

--- coinstar.py
+++ coinstar.py
@@ -25,13 +25,10 @@ class Market():
         self.prices = self.transactions['prices']
         self.volumes = self.transactions['total_volumes']
 
-        # self.market_days = self.create_days()
+        self.market_days = self.create_days()
 
 
     def bearish_length(self):
-        pass
-
-    def is_bearish(self):
         pass
 
     def find_max_volume(self):
@@ -51,30 +48,51 @@ class Market():
         return r.json()
 
     def create_days(self):
-        start_of_day = self.time_from
-        last_value = 0
+        MS_IN_DAY = self.SECONDS_IN_DAY * 1000
+        start_of_day = self.time_from * 1000                        # Convert timestamp to ms
+        prices = self.prices
+        volumes = self.volumes
+
+        TIMESTAMP = 0
+        VALUE = 1
+
+        last_day_close_value = 0
         market_days = []
-        day_volume = []
-        for datapoint in self.prices:
-            if datapoint[0] > start_of_day:
-                start_of_day += self.SECONDS_IN_DAY
-                start_value = datapoint[1]
+        day_volume = 0
+        first_day = True                                            # Is this the first day in range?
+        bearish = False
 
-        try:
-            bearish = market_days[-1].close_value > end_value
+        for i in range(len(prices)):
 
-        except KeyError:
-            # If day is the first day, previous day is not defined
-            bearish = False
+            print(f"Timestamp {prices[i][TIMESTAMP]}")
+            print(f"Day Start {start_of_day}")
+            if prices[i][TIMESTAMP] > start_of_day:                 # Look for the start of a new day
+                day_end_value = prices[i-1][VALUE]                  # previous datapoint was last days last datapoint.
 
-        market_days.append( Market_day(start_value, end_value, volume, bearish) )
+                if not first_day:
+                    is_bearish = last_day_close_value > day_end_value
+                    market_days.append( Market_day(day_start_value, day_end_value, day_volume, is_bearish) )
+
+                    # Setup variables for next day
+                    last_day = market_days[-1]
+                    last_day_close_value = last_day.close_value
+
+                # Reset day variables to a new day
+                day_start_value = prices[i][VALUE]
+                day_volume = volumes[i][VALUE]
+
+                first_day = False
+                start_of_day += MS_IN_DAY                      # Next day starts in 24h
+
+            else:
+                day_volume += volumes[i][VALUE]                     # Add datapoint volume to the days volume
 
         return market_days
 
 
 class Market_day():
 
-    def __init__(self, start, end, volume, bearish):
+    def __init__(self, start, end, volume=None, bearish=False):
 
         self.start_value = start
         self.close_value = end
@@ -92,12 +110,19 @@ def time_to_posix(year, mon, day, hour=0, min=0, sec=0):
 if __name__ == "__main__":
     bitcoin_market = Market(
         time_from = time_to_posix(2021, 11, 22),
-        time_to = time_to_posix(2021, 11, 23),
+        time_to = time_to_posix(2021, 11, 22),
         coin="bitcoin",
         currency="eur"
     )
-    print(f"Prices:")
-    print(json.dumps(bitcoin_market.prices, indent=4, sort_keys=True))
+
+    for day in bitcoin_market.market_days:
+        print(f"Open: {day.start_value} Close: {day.close_value} " \
+              f"Volume: {day.trading_volume} Bearish: {day.is_bearish}")
+
+
+
+    # print(f"Prices:")
+    # print(json.dumps(bitcoin_market.prices, indent=4, sort_keys=True))
 
     # print(f"volumes:")
     # print(json.dumps(bitcoin_market.volumes, indent=4, sort_keys=True))
