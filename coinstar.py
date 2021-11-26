5c945e3915825cb49dd2f41264c5785197ed6655
JValtteri <46611993+JValtteri@users.noreply.github.com>
2021-11-26 19:27:15 +0200

    Added input arguments
    -s -e -r -h
    for use in CLI

--- coinstar.py
+++ coinstar.py
@@ -5,6 +5,7 @@
 
 import json
 import datetime
+import sys, getopt
 from market import Market
 
 
@@ -19,17 +20,58 @@ def print_datapoints(datapoints, title="datapoints"):
     print(title)
     print(json.dumps(datapoints, indent=4, sort_keys=True))
 
-if __name__ == "__main__":
+
+def main(argv):
+    """Main function"""
+    # Parse arguments
+
+    helptext = 'coinstar.py <opts>\n\n' \
+               '-s    --start  <start date> YYYY.MM.DD\n' \
+               '-e    --end    <end date>   YYYY.MM.DD\n' \
+               '-r    --raw                 Show raw data\n' \
+               '-h    --help                Show this help\n'
+    print_raw = False
+
+    try:
+        opts, args = getopt.getopt(argv,"hrs:e:",["start=","end=","help"])
+    except getopt.GetoptError:
+        print ('coinstar.py -s <start date> -e <end date> -r --help')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ('-h', 'help') or len(argv) is 0:
+            print (helptext)
+            sys.exit()
+        elif opt in ("-s", "--start"):
+            start_str = arg
+        elif opt in ("-e", "--end"):
+            end_str = arg
+        elif opt in ("-r", "--raw"):
+            print_raw = True
+
+    if opts == []:
+        print ('coinstar.py -s <start date> -e <end date> -r --help')
+        sys.exit(2)
+
+    start = [int(i) for i in start_str.split('.')]
+    end = [int(i) for i in end_str.split('.')]
+
+    # Process market data
     market = Market(
-        time_from = time_to_posix(2020, 1, 19),
-        time_to = time_to_posix(2020, 1, 21),
+        time_from = time_to_posix(start[0], start[1], start[2]),
+        time_to = time_to_posix(end[0], end[1], end[2]),
         coin="bitcoin",
         currency="eur"
     )
 
+    # Print program outputs
+    print(f"Start date: {start_str}")
+    print(f"End date: {end_str}")
+
     market.print_days()
     print(f"Max Bearish: {market.longest_bearish}")
+    if print_raw:
+        print_datapoints(market.prices, "Prices")
+        print_datapoints(market.volumes, "Volumes")
 
-
-    print_datapoints(market.prices, "Prices")
-    print_datapoints(market.volumes, "Volumes")
+if __name__ == "__main__":
+    main(sys.argv[1:])
