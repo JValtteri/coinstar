fd106f30e7ea43fa61f2deea60bbbacfdef2d3b7
JValtteri <46611993+JValtteri@users.noreply.github.com>
2021-11-26 21:57:08 +0200

    Basic gui layout

--- gui.py
+++ gui.py
@@ -27,21 +27,31 @@ class Gui():
         # l_title = Label(root, text="Oh wow!")
         # l_title.grid(row=0, column=0)
 
+
+        # BUTTONS
+
         btn_close = Button(root, text="Close", command=close, bg="#C00000", padx=10, pady=10)
-        btn_close.grid(row=90)
+        btn_close.grid(row=90, column=2)
+
+        btn_get = Button(root, text="Get", command=self.get, bg="#00C000", padx=30, pady=15)
+        btn_get.grid(row=90, column=1, padx=0, pady=5)
+
+        # LABELS
 
         l_start = Label(root, text="Start date")
         l_start.grid(row=2, column=0)
 
-        self.e_start = Entry(root, width=50)#, bg="#808080", fg="#000000")
-        self.e_start.grid(row=3, column=0)
-        self.e_start.insert(0, "YYYY.MM.DD")
-
         l_end = Label(root, text="End date")
         l_end.grid(row=4, column=0)
 
+        # ENTRY FIELDS
+
+        self.e_start = Entry(root, width=50)#, bg="#808080", fg="#000000")
+        self.e_start.grid(row=3, column=0, columnspan=3, padx=10, pady=10)
+        self.e_start.insert(0, "YYYY.MM.DD")
+
         self.e_end = Entry(root, width=50)#, bg="#808080", fg="#000000")
-        self.e_end.grid(row=5, column=0)
+        self.e_end.grid(row=5, column=0, columnspan=3, padx=10, pady=10)
         self.e_end.insert(0, "YYYY.MM.DD")
 
         # Start program
