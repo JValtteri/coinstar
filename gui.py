c9248c8fbbb3942f117d26fe179bd90e1922ff84
JValtteri <46611993+JValtteri@users.noreply.github.com>
2021-11-26 21:47:48 +0200

    Created Gui Class and added elements

--- gui.py
+++ gui.py
@@ -6,46 +6,56 @@
 import sys
 from  tkinter import *
 
+class Gui():
+
+    def __init__(self):
+        pass
+
+        self.run()
+
+    def run(self):
+
+        # Setup the window
+        root = Tk()
+        root.title("Coinstar GUI")
+        # root.configure(bg="#050505")
+        # root.geometry("400x200")
+
+        # frame = Frame(root, padding=10)
+        # frame.grid()
+
+        # l_title = Label(root, text="Oh wow!")
+        # l_title.grid(row=0, column=0)
+
+        btn_close = Button(root, text="Close", command=close, bg="#C00000", padx=10, pady=10)
+        btn_close.grid(row=90)
+
+        l_start = Label(root, text="Start date")
+        l_start.grid(row=2, column=0)
+
+        self.e_start = Entry(root, width=50)#, bg="#808080", fg="#000000")
+        self.e_start.grid(row=3, column=0)
+        self.e_start.insert(0, "YYYY.MM.DD")
+
+        l_end = Label(root, text="End date")
+        l_end.grid(row=4, column=0)
+
+        self.e_end = Entry(root, width=50)#, bg="#808080", fg="#000000")
+        self.e_end.grid(row=5, column=0)
+        self.e_end.insert(0, "YYYY.MM.DD")
+
+        # Start program
+        root.mainloop()
 
 
-def start():
+    def get(self):
 
-    # Setup the window
-    root = Tk()
-    root.title="Coinstar GUI"
-    # root.configure(bg="#050505")
-    # root.geometry("400x200")
-
-    # frame = Frame(root, padding=10)
-    # frame.grid()
-
-    # l_title = Label(root, text="Oh wow!")
-    # l_title.grid(row=0, column=0)
-
-    btn_close = Button(root, text="Close", command=close, bg="#C00000", padx=10, pady=10)
-    btn_close.grid(row=10)
-
-    l_start = Label(root, text="Start date")
-    l_start.grid(row=2, column=0)
-
-    e_start = Entry(root, width=50)#, bg="#808080", fg="#000000")
-    e_start.grid(row=3, column=0)
-
-    l_end = Label(root, text="End date")
-    l_end.grid(row=4, column=0)
-
-    e_end = Entry(root, width=50)#, bg="#808080", fg="#000000")
-    e_end.grid(row=5, column=0)
-
-    # Start program
-    root.mainloop()
-
-
-def get():
-    pass
+        start = self.e_start.get()
+        end = self.e_end.get()
 
 def close():
     sys.exit()
 
 if __name__ == "__main__":
-    start()
+    gui = Gui()
+
