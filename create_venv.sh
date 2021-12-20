1dc239e4cf215916fe28d79599965f2eb78d1ee6
JValtteri <46611993+JValtteri@users.noreply.github.com>
2021-12-20 12:12:01 +0200

    Fixed: call python3 explicitly

--- create_venv.sh
+++ create_venv.sh
@@ -6,7 +6,7 @@ echo "Installing Python3-venv"
 sudo apt-get install -y python3-venv
 
 echo "Creating venv"
-python -m venv venv
+python3 -m venv venv
 source venv.sh
 ./install.sh
 pip3 install pyinstaller
